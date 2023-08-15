37df0d0e5b0629b2b09dda23e06056ae4e980f9d
leo31415926 <64792861+leo31415926@users.noreply.github.com>
2023-08-15 17:45:34 +0800

    Add files via upload

---
+++ 6_💌聊天机器人.py
@@ -0,0 +1,37 @@
+import openai
+import streamlit as st
+
+st.title("ChatGPT-聊天机器人")
+
+openai.api_key = "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"
+
+if "openai_model" not in st.session_state:
+    st.session_state["openai_model"] = "gpt-3.5-turbo"
+
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+if prompt := st.chat_input("输入您的问题"):
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    with st.chat_message("user"):
+        st.markdown(prompt)
+
+    with st.chat_message("assistant"):
+        message_placeholder = st.empty()
+        full_response = ""
+        for response in openai.ChatCompletion.create(
+            model=st.session_state["openai_model"],
+            messages=[
+                {"role": m["role"], "content": m["content"]}
+                for m in st.session_state.messages
+            ],
+            stream=True,
+        ):
+            full_response += response.choices[0].delta.get("content", "")
+            message_placeholder.markdown(full_response + "▌")
+        message_placeholder.markdown(full_response)
+    st.session_state.messages.append({"role": "assistant", "content": full_response})
